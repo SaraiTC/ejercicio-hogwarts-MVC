7c35601cd252190a1c509895b4c10ba0f3b0aec0
sarai_dev <89134308+SaraiTC@users.noreply.github.com>
2022-01-05 09:01:44 +0100

    commit

--- view/admin/partials/footer.php
+++ view/admin/partials/footer.php
@@ -2,8 +2,8 @@
         </main>
         <footer class="center-align">
             © <?php echo date("Y") ?> Panel de Administración.
-            <a href="https://jairogarciarincon.com" target="_blank" title="Jairo García Rincón">
-                Jairo García Rincón
+            <a href="#" target="_blank" title="Sarai Temes">
+                Sarai Formaweb
             </a>
         </footer>
 
